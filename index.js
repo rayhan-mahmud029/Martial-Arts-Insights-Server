df43e5259897bbcaf8145884bd25950e9db76065
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-06-15 22:37:39 +0600

    express server running

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('martial arts insights server running')
+})
+
+
+app.listen(port, () => {
+    console.log('Server running in port ', port);
+})
